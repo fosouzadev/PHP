f7254505baf982f3dd79f997b8c3f8b36335bd54
Felipe <fo.souza@outlook.com>
2021-01-29 08:45:19 -0300

    estudo php

---
+++ CursosAlura/Avançado1/arrays.php
@@ -0,0 +1,10 @@
+<?php
+
+# forma antiga de declarar array
+$idades1 = array(21, 23, 19, 25, 30, 41, 18);
+
+# forma atual de declarar array
+$idades2 = [21, 23, 19, 25, 30, 41, 18];
+
+
+echo count($idades2);
