ac8670f8076078dbfac2d472ce3c01abdb100712
Felipe <fo.souza@outlook.com>
2021-02-03 08:49:06 -0300

    estudo php

--- CursosAlura/2_Avançando/functions.php
+++ CursosAlura/2_Avançando/functions.php
@@ -20,10 +20,23 @@ function helloWorld4($msg) {
     return $msg . PHP_EOL;
 }
 
-# parametros tipados
-function helloWorldType(string $msg, float $value) {
+# parametros e retorno tipados
+function helloWorldType(string $msg, float $value) : float {
+    echo "$msg $value" . PHP_EOL;
+    return 0;
+}
+function helloWorldType2(string $msg, float $value) : void {
     echo "$msg $value" . PHP_EOL;
 }
+
+# parametro e retorno tipado ou null
+function helloWorldTypeNull(?float $numero) : ?float {
+    if ($numero !== null)
+        return $numero * 2;
+    
+    return $numero;
+}
+
 
 helloWorld();
 echo helloWorld2();
@@ -31,3 +44,5 @@ helloWorld3('hello world!');
 echo helloWorld4('hello world!');
 helloWorldType('hello world', 2021);
 helloWorldType(true, 2021);
+echo helloWorldTypeNull(5);
+echo helloWorldTypeNull(null);
