51471adacc18e877c4fcf4753823fba869770425
Felipe <fo.souza@outlook.com>
2021-02-09 07:14:39 -0300

    estudo php

--- CursosAlura/4_AvançandoOO/FuncionarioTest1.php
+++ CursosAlura/4_AvançandoOO/FuncionarioTest1.php
@@ -4,9 +4,11 @@ namespace alura\banco;
 
 require 'autoload.php';
 
-use alura\banco\models\{ Funcionario, Cpf };
+use alura\banco\models\{ Cpf };
+use alura\banco\models\funcionario\Funcionario;
 use alura\banco\util\Functions;
 
+// não permite instanciar porque é uma classe abstrata
 $func1 = new Funcionario('Paulo', new Cpf('222'), 'Analista');
 $func2 = new Funcionario('Sarai', new Cpf('333'), 'Atendimento');
 
