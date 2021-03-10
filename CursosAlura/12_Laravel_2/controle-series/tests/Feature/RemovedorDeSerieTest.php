a60b449f7fa98e7f97e94829f562860c0c64557b
Felipe <fo.souza@outlook.com>
2021-03-10 09:14:02 -0300

    Estudo Laravel 2

---
+++ CursosAlura/12_Laravel_2/controle-series/tests/Feature/RemovedorDeSerieTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Serie;
+use App\Services\CriadorDeSerie;
+use App\Services\RemovedorDeSerie;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RemovedorDeSerieTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @var Serie */
+    private $serie;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $criadorDeSerie = new CriadorDeSerie();
+        $this->serie = $criadorDeSerie->criarSerie(
+            'Nome da série',
+            1,
+            1);
+
+    }
+
+    public function testRemoverUmaSerie()
+    {
+        $this->assertDatabaseHas('series', ['id' => $this->serie->id]);
+
+        $removedorDeSerie = new RemovedorDeSerie();
+        $serie = $removedorDeSerie->removerSerie($this->serie->id);
+
+        $this->assertInstanceOf(Serie::class, $serie);
+        //$this->assertIsString($serie);
+        $this->assertEquals('Nome da série', $this->serie->nome);
+        $this->assertDatabaseMissing('series', ['id' => $this->serie->id]);
+
+    }
+}
